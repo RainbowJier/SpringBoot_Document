ca3342b0626f14922822280b8864fc0f2b443ef4
RianbowJier <3021809270@qq.com>
2024-04-11 10:52:42 +0800

    update

---
+++ SpringBoot_Test/src/test/java/com/example/SpringBoot_Test/SpringBoot_TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot_TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
